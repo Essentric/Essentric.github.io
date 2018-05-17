bb35d08746680a8e010c593b9bd6bbb703125019
Essentric <essentric@foxmail>
2018-05-17 08:46:33 +0800

    修改版

--- js/custom.js
+++ js/custom.js
@@ -1,36 +1,28 @@
 /*------------------------------------------------------------------
-Project:        YellowMoon - HTML onepage theme
-Version:        1.0
-Last change:    08/08/2017
-Author:         GraphBerry
-URL:            #
-License:        #/pages/license
+Project:        resume
+Author:         Essentric
+URL:            https://essentric.github.io/resume/
 -------------------------------------------------------------------*/
+
 $(function(){
+    // 严格模式
 	'use strict';
 
-
-	/*--------------------------------------------------
-    Scrollspy Bootstrap 
-    ---------------------------------------------------*/
-
+    // Scrollspy 插件
     $('body').scrollspy({
     	target: '#main-navbar',
     	offset: 121
     });
 
+    /*------------------------------------------------------------------------------------*/
 
-    /*--------------------------------------------------
-    Affix Bootstrap 
-    ---------------------------------------------------*/ 
-
+    // affix 插件
     $('#main-navigation').affix({
         offset: {
             top: function() {
+                // 页面当前高度
                 var windowHeight = $(window).height();
-                // Calculate distance from the top 
-                // in accordance with home section height
-                if(windowHeight > 568 & windowHeight < 1050) {
+                if(windowHeight > 568 && windowHeight < 1050) {
                     return windowHeight;
                 } else if(windowHeight <= 568) {
                     return 568;
@@ -41,22 +33,20 @@ $(function(){
         }
     });
 
+	/*------------------------------------------------------------------------------------*/
 
-	/*--------------------------------------------------
-    Smooth Scroll 
-    ---------------------------------------------------*/
-
+    // Smooth Scroll 插件
 	smoothScroll.init({
 		selector: '.smoothScroll',
 		speed: 1000,
 		offset: function(){
-            // Check if mobile navigation is active
+
             var query = Modernizr.mq('(max-width: 767px)');
-            // Check if navbar is fixed
+
             var navFix = $("#main-navigation").hasClass('affix');
-            // Check if navbar-collapse is not collapsed
+
             var navExt = $('#main-navbar').hasClass('in');
-            // Calculate offset
+
             if(query && navExt && !navFix) {
                 return (121 + $("#main-navbar").height());
             } else {
@@ -64,49 +54,49 @@ $(function(){
             }
         },
 		before: function(anchor, toggle){
-			// Check if mobile navigation is active
+
 			var query = Modernizr.mq('(max-width: 767px)');
-			// Check if element is navigation eelement
+
 			var navItem = $(toggle).parents("#main-navbar").length;
-			// If mobile nav & nav item then close nav
+
 			if (query && navItem !== 0) {
 				$("button.navbar-toggle").click();
 			}
 		}
 	});
 
-    /*--------------------------------------------------
-    Mobile navigation fix
-    ---------------------------------------------------*/
-    
+    /*------------------------------------------------------------------------------------*/
+
+    // 解决移动导航
     var setNavHeight = function(){
-        // Check if mobile navigation is active
         var query = Modernizr.mq('(max-width: 767px)');
-        // Set navbar-collapse maxHeight
+        // 设置 navbar-collapse 的最大高度
         if(query) {
-            $(".navbar-collapse").css({ 
-                maxHeight: $(window).height() - $(".navbar-header").height() + "px" 
+            $(".navbar-collapse").css({
+                maxHeight: $(window).height() - $(".navbar-header").height() + "px"
             });
         }
     };
 
-    // Trigger on init
     setNavHeight();
 
-    // Listen for resize (orientation) changes
+    // 监听高度改变
     $(window).resize(function(){
         setNavHeight();
     });
 
+	/*------------------------------------------------------------------------------------*/
 
-	/*--------------------------------------------------
-    Current Year
-    ---------------------------------------------------*/
-
-    // Automatically update copyright year in the footer
+    // 自动更新脚注的年份
 	var currentTime = new Date();
 	var year = currentTime.getFullYear();
 	$("#year").text(year);
 
-
+    // progress插件
+    var progress = $(".progress-bar-inner");
+    progress.each(function (i)
+    {
+        var data = $(this).attr('data-value');
+        $(this).prev().find("span").html(data+"%");
+    });
 });
